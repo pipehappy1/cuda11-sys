de89834f4f3e3e516a363591bda474dde98d0894
yguan <yguan@example.com>
2020-08-03 01:18:25 +0800

    add error checker

--- cuda11-cudart-sys/src/lib.rs
+++ cuda11-cudart-sys/src/lib.rs
@@ -3,6 +3,12 @@ mod cudart;
 pub use crate::cudart::*;
 
 
+fn checkCudaStatus(status: cuda11_cudart_sys::cudaError_t ) {
+    if status != cuda11_cudart_sys::cudaError::cudaSuccess {
+        print!("cuda API failed with status \n");
+        panic!();
+    }
+}
 
 
 #[cfg(test)]
